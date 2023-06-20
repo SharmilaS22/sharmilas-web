1f857904b682d3d76c656010148724fed5a7c19b
Sharmila S <sharmilas@Sharmilas-MacBook-Pro.local>
2023-06-20 21:40:58 +0530

    [Sharmi] Fix lint

--- src/components/layouts/Content/About.jsx
+++ src/components/layouts/Content/About.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function About() {
-    const aboutText = [
-        "Software engineer from India. I like learning and applying computer technologies to develop websites, applications and other software programs.",
-        "I am also a part-time blogger. I write articles on Medium about Node.js, AWS, CSS and related topics.",
-        "I enjoy reading Sci-fi and Fantasy novels. Designing digital planners is one of my creative interests."
-    ]
+    // const aboutText = [
+    //     "Software engineer from India. I like learning and applying computer technologies to develop websites, applications and other software programs.",
+    //     "I am also a part-time blogger. I write articles on Medium about Node.js, AWS, CSS and related topics.",
+    //     "I enjoy reading Sci-fi and Fantasy novels. Designing digital planners is one of my creative interests."
+    // ]
 
     const aboutTextUpdated = [
         "I am a passionate software engineer from India, dedicated to learning and applying computer technologies to develop websites, applications, and software programs.",
